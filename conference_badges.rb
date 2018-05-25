35cba35ef3de64fb92e4e10640c5b23db2f52fc6
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-25 01:15:51 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker_list)
+  new_array = []
+  counter = 0
+  while counter <= speaker_list.length - 1
+    new_array.push("Hello, my name is #{speaker_list[counter]}.")
+    counter += 1
+  end
+  new_array
+end
+
+def assign_rooms(speaker_list)
+  room_assignments = []
+  speaker_list.each_with_index { | speaker, index |
+    room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{index +1}!")
+  }
+  room_assignments
+end
+
+def printer(speaker_list)
+  counter = 0
+  while counter < speaker_list.length
+    puts batch_badge_creator(speaker_list)[counter]
+    puts assign_rooms(speaker_list)[counter]
+    counter +=1
+  end
+end
